1cea818e056981d0a250fd962f725134b1276c61
Cotten-Lumb03 <cottenlumb@ufl.edu>
2023-07-18 17:00:18 -0400

    Working on Encode Function

--- cotten_encoder.py
+++ cotten_encoder.py
@@ -1,1 +1,13 @@
-print("Do you see this message?")
+def main_menu():
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3, Quit")
+
+
+def encode(user_password):
+    password_output = ''
+    for num in str(user_password):
+        if num == '0':
+            num = str(3)
+        else:
+            num = str(int(num) + 3)
+            if len(num) == 2:
+                num = num[-1]
